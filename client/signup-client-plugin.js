24c11f62c73580753e251a0ace5281622699f961
Rigel Kent <sendmemail@rigelk.eu>
2019-11-22 00:59:40 +0100

    recaptcha v2 for registration

---
+++ client/signup-client-plugin.js
@@ -0,0 +1,49 @@
+import loadjs from "loadjs"
+
+function register({ registerHook, peertubeHelpers }) {
+  initRC(registerHook, peertubeHelpers).catch(err =>
+    console.error("Cannot initialize recaptcha plugin", err)
+  )
+}
+
+export { register }
+
+function initRC(registerHook, peertubeHelpers) {
+  return peertubeHelpers.getSettings().then(async s => {
+    if (!s || !s["recaptcha-site-key"]) {
+      console.error(
+        "Recaptcha plugin lacks a site key set to use the ReCaptcha. Falling back to normal registration."
+      )
+      return
+    }
+
+    // add captcha to the first form (user form)
+    const node = document.getElementsByTagName("form")[0]
+    const div = document.createElement("div")
+    div.setAttribute("class", "g-recaptcha")
+    node.appendChild(div)
+
+    // global callback definition is the most flexible way, since
+    // loadjs's callback triggers too soon: immediately after the
+    // resource load, when in fact api.js loads other resources
+    // before it is defining grecaptcha.
+    window.onReCaptchaLoadCallback = () => {
+      grecaptcha.render(div, {
+        callback: () =>
+          (window.ReCaptchaLoadCallbackResponse = grecaptcha.getResponse()),
+        sitekey: s["recaptcha-site-key"]
+      })
+    }
+    loadjs(
+      "https://www.google.com/recaptcha/api.js?onload=onReCaptchaLoadCallback&render=explicit"
+    )
+
+    registerHook({
+      target: "filter:signup.register.body",
+      handler: body =>
+        Object.assign({}, body, {
+          "g-recaptcha-response": window.ReCaptchaLoadCallbackResponse
+        })
+    })
+  })
+}
---
+++ main.js
@@ -0,0 +1,85 @@
+var _request = require("request")
+
+const request = function(options) {
+  return new Promise((resolve,reject) => {
+    _request(options, (error, response, body) => {
+      if (response) {
+        return resolve(response)
+      }
+      if (error) {
+        return reject(error)
+      }
+    })
+  })
+}
+
+async function register({
+  registerHook,
+  registerSetting,
+  settingsManager
+}) {
+  // see https://developers.google.com/recaptcha/docs/faq#id-like-to-run-automated-tests-with-recaptcha.-what-should-i-do
+  /* 
+    Test Site key: 6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI
+    Test Secret key: 6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe
+    They will always return a validated response, and will show a captcha with a warning
+  */
+  registerSetting({
+    name: 'recaptcha-site-key',
+    label: 'ReCaptcha Site Key',
+    type: 'input',
+    private: false
+  })
+  registerSetting({
+    name: 'recaptcha-secret-key',
+    label: 'ReCaptcha Secret Key',
+    type: 'input',
+    private: true
+  })
+
+  registerHook({
+    target: "filter:api.user.signup.allowed.result",
+    handler: (result, params) => verifyCaptcha(result, params, settingsManager)
+  })
+}
+
+async function unregister() {
+  return
+}
+
+module.exports = {
+  register,
+  unregister
+}
+
+async function verifyCaptcha (result, params, settingsManager) {
+  // g-recaptcha-response is the key that browser will generate upon form submit.
+  // if its blank or null means user has not selected the captcha, so return the error.
+  if (
+    params.body["g-recaptcha-response"] === undefined ||
+    params.body["g-recaptcha-response"] === "" ||
+    params.body["g-recaptcha-response"] === null
+  ) {
+    return { allowed: false, errorMessage: "Captcha wasn't filled" }
+  }
+
+  const secretKey = await settingsManager.getSetting('recaptcha-secret-key')
+  if (!secretKey) return result
+
+  // params.connection.remoteAddress will provide IP address of connected user.
+  const verificationUrl =
+    "https://www.google.com/recaptcha/api/siteverify?secret=" +
+    secretKey +
+    "&response=" +
+    params.body["g-recaptcha-response"] +
+    "&remoteip=" +
+    params.connection.remoteAddress
+
+  return await request(verificationUrl, (error, response, body) => {
+    body = JSON.parse(body)
+    if (body.success !== undefined && !body.success) {
+      return { allowed: false, errorMessage: "Wrong captcha" }
+    }
+    return result
+  })
+}
